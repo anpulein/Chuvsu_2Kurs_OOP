6285841ba9e0fb33b1b4359fcb409d26c3fdb345
Dmitry <dastalein@gmail.com>
2022-05-08 22:18:36 +0300

    predict

--- Coursework/Headers/Expert.h
+++ Coursework/Headers/Expert.h
@@ -5,4 +5,19 @@
 #ifndef CHUVSU_2KURS_OOP_EXPERT_H
 #define CHUVSU_2KURS_OOP_EXPERT_H
 
+#include <map>
+#include <string>
+using namespace std;
+
+struct problems {
+    string name;
+};
+
+class Expert {
+    private:
+        map<string, bool> dictQuestions;
+
+
+};
+
 #endif //CHUVSU_2KURS_OOP_EXPERT_H
--- Coursework/Headers/KnowledgeBase.h
+++ Coursework/Headers/KnowledgeBase.h
@@ -5,4 +5,30 @@
 #ifndef CHUVSU_2KURS_OOP_KNOWLEDGEBASE_H
 #define CHUVSU_2KURS_OOP_KNOWLEDGEBASE_H
 
+#include <map>
+#include <string>
+#include <list>
+using namespace std;
+
+struct problem {
+    string name;
+    list<string> questians;
+};
+
+class KnowledgeBase {
+    private:
+        map<string, bool> dictQuestions = {{"Отсутствует реакция на нажатие кнопки включения? ", false},
+                                           {"Все кабели питания подключены правильно? ", false},
+                                           {"Провода и коннекторы внетри корпуса пк исправны? ", false},
+                                           {"При отсоединении кнопки перезагрузки от мат.платы компьютер начинает работать? ",false},
+                                           {"При замыкании двух контактов с надписью 'Power Swirch' происходит запуск компьютера? ", false},
+                                           {"Включенный в сеть блок питания не начинает работать при замыкании черного и зеленого контактов? ", false},
+                                           {"При нажатии кнопки запуска компьютер начинает работать? ", false},
+                                           {"Компьютер сразу прекращает работу? ", false},
+                                           {"В процессе загрузки компьютер подает характерный сигнал? ", false}};
+        list<problem> problems;
+
+
+};
+
 #endif //CHUVSU_2KURS_OOP_KNOWLEDGEBASE_H
