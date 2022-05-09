a54f5798f45530f041528ce822a3a0261b47622c
Dmitry <dastalein@gmail.com>
2022-05-09 09:11:08 +0300

    Method - showAnswer
    Added a method that gives the result to the user

--- Coursework/Headers/Expert.h
+++ Coursework/Headers/Expert.h
@@ -15,19 +15,31 @@ using namespace std;
 
 class Expert : public KnowledgeBase{
     private:
-        bool isProblemSolved = false;
-
-        void findAnswer() {
+        int findAnswer() {
             for (int i = 0; i < problems.size(); ++i) {
                 if (findAnswerInProblems(i)) {
-                    isProblemSolved = true;
-                    break;
+                    return i;
                 }
             }
-
+            return -1;
         }
 
-        
+        void showAnswer() {
+            string str;
+            int answer = findAnswer();
+            cout << "Процесс выявления проблемы завершен. ";
+            if (answer == -1) {
+                cout << "По полученным ответам в моей базе знаний не существует подобной проблемы\n Хотели бы вы добавить новую проблему в мою базу знаний? " << endl << "Ответ: ";
+                cin >> str;
+                
+                if (str.find("Да") || str.find("да")) { iter->second = true; }
+                else if (str.find("Нет") || str.find("нет")) { iter->second = false; }
+            } else {
+                cout << "Вашей проблемой может являться: " << problems.at(answer).name << endl;
+            }
+
+            cout << "Спасибо, что воспользовались нашей системой обслуживания. Хорошего дня:)" << endl;
+        }
 
     public:
         Expert() = default;
