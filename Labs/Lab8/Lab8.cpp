bba3003e523db738ab22ba0e858b853d203636eb
Dmitry <dastalein@gmail.com>
2022-05-10 10:31:33 +0300

    start lab

--- Labs/Lab8/Lab8.cpp
+++ Labs/Lab8/Lab8.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
 using namespace std;
 
+void example_show(short i) {
+    cout << "/**************************** Example " << i
+         << "****************************/" << endl;
+}
+
 int main() {
-    
+
+    {
+        example_show(1);
+
+
+    }
+
+    {
+        example_show(2);
+
+
+    }
+
+    {
+        example_show(3);
+
+
+    }
+
+    {
+        example_show(4);
+
+
+    }
+
+    {
+        example_show(5);
+
+
+    }
+
+    {
+        example_show(6);
+
+
+    }
+
+    {
+        example_show(7);
+
+
+    }
+
+    {
+        example_show(8);
+
+
+    }
+
+    {
+        example_show(9);
+
+
+    }
+
+    {
+        example_show(10);
+
+
+    }
+
+    {
+        example_show(11);
+
+
+    }
+
+    {
+        example_show(12);
+
+
+    }
+
 }
